4c119f75404f6f591e7183ba0fbbb1c59a2bdb2c
Lauri <latenssi@gmail.com>
2020-03-31 14:34:15 +0300

    Add devnet config

--- truffle.js
+++ truffle.js
@@ -7,6 +7,11 @@ module.exports = {
       port: 7545,
       network_id: "*"
     },
+    devnet: {
+      network_id: "*",
+      from: "0x00a329c0648769A73afAc7F9381E08FB43dBEA72",
+      provider: new Web3.providers.HttpProvider('http://ethereum-sprawl-testnet:8090')
+    },
     testnet: {
       network_id: "*",
       from: "0x00a329c0648769A73afAc7F9381E08FB43dBEA72",
